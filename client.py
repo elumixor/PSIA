483e59212e524728588e21b780ace8f8a26ce0ce
tkraus7 <76591708+tkraus7@users.noreply.github.com>
2021-02-24 21:01:34 +0100

    implemented simple image sharing function

--- client.py
+++ client.py
@@ -2,13 +2,17 @@ import socket
 
 port = 5300
 ip = "127.0.0.1"
+SIZE = 1024
 
 if __name__ == '__main__':
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    message = b"Hello world"  # send as bytes
-    client_socket.sendto(message, (ip, port))
 
-    # Receive response
-    data, server = client_socket.recvfrom(1024)
+    file = open("original.jpg", "rb")
+    message = file.read(SIZE)
 
-    print(f"Received {data} from {server}")
+    while message:
+        client_socket.sendto(message, (ip, port))
+        message = file.read(SIZE)
+
+    file.close()
+    client_socket.close()
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import socket
 
 port = 5300
-ip = ""
+ip = "127.0.0.1"
+SIZE = 1024
 
 if __name__ == '__main__':
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((ip, port))
 
-    message, address = server_socket.recvfrom(1024)
-    print(f"Received: {message} from {address}")
+    file = open('received.jpg', 'wb')
+    message, address = server_socket.recvfrom(SIZE)
+    while message:
+        file.write(message)
+        message, address = server_socket.recvfrom(SIZE)
 
-    # Reply
-    server_socket.sendto(message, address)
+    file.close()
+    server_socket.close()
